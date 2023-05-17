b45b5b02b5e497e4ccb9d38945c0f33cf409b4e3
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-17 23:26:58 +0600

    MongoDb connected with server

--- index.js
+++ index.js
@@ -12,6 +12,40 @@ app.use(express.json());
 
 
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toysCollection = client.db("mightyMarvelousToys").collection("allToys");
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Server successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
